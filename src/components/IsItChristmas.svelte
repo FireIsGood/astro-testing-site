0e10563cf0765fefa867712ea504f11ca16281c9
FireIsGood <fireisadequate@gmail.com>
2022-09-22 09:07:17 -0700

    Add Christmas checker and blog

---
+++ src/components/IsItChristmas.svelte
@@ -0,0 +1,12 @@
+<script>
+  const today = new Date();
+  const thisYear = today.getFullYear();
+  const christmas = new Date(`${thisYear} December 31`);
+
+</script>
+
+{#if today = christmas}
+  <h1>YES</h1>
+{:else}
+  <h1>No</h1>
+{/if}

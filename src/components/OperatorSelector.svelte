cfa332ebbf1c0687867c726b875166314c873087
FireIsGood <fireisadequate@gmail.com>
2022-09-22 09:06:55 -0700

    Add Svelte Autocomplete and example

---
+++ src/components/OperatorSelector.svelte
@@ -0,0 +1,19 @@
+<script>
+  import AutoComplete from "simple-svelte-autocomplete";
+
+  const items = [
+    { id: 1, name: "Surtr" },
+    { id: 2, name: "Eyja" },
+    { id: 3, name: "Thorns" },
+  ];
+
+  let selectedItem;
+</script>
+
+<br>
+<AutoComplete items={items} bind:selectedItem={selectedItem} >
+</AutoComplete>
+
+{#if selectedItem}
+  <p>{selectedItem.name} with ID {selectedItem.id}</p>
+{/if}
